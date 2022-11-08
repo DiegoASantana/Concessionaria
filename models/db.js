6a03d3399069eab82b057d97710817a0ee0de514
Diego Santana <81784762+DiegoASantana@users.noreply.github.com>
2022-11-07 23:49:26 -0300

    Atualização da ligação de acesso ao banco no heroku
    foi necessário acessar o arquivo db.js e mudar os dados do banco que estavam lá, para os dados do banco no heroku.
    
    host: us-cdbr-east-06.cleardb.net
    usuario: b30ac26808c0a7
    senha: d0543e7f
    Banco: heroku_c61f56fe42d68dc

--- models/db.js
+++ models/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 //Conexão com o banco de dados MySql
-    const sequelize = new Sequelize('mydb', 'root', '151819', {
-        host: "localhost",
+    const sequelize = new Sequelize('heroku_c61f56fe42d68dc', 'b30ac26808c0a7', 'd0543e7f', {
+        host: "us-cdbr-east-06.cleardb.net",
         dialect: 'mysql',
         query:{raw:true}
     });
