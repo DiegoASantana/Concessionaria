444493408ab787fbdeda7a189ab6b13ece0fd00f
Diego Santana <81784762+DiegoASantana@users.noreply.github.com>
2022-11-17 01:30:44 -0300

    Criação da Página de Vendas

--- Routes/rota_vendas.js
+++ Routes/rota_vendas.js
@@ -5,12 +5,23 @@ const Veiculo = require('../models/Veiculo');
 const Cliente = require('../models/Cliente');
 const VeiculoVendido = require ('../models/VeiculoVendido');
 
+//Veiculo.hasMany(Venda)
+//Venda.belongsTo(Veiculo,{foreignKey: 'VEN_IdVeiculo'});
+//Venda.belongsTo(Cliente, {foreignKey: 'VEN_IdCliente'});
+
+router.get('/', (req,res)=>{
+    res.render('vendas');
+})
+
+
+
+
 
 router.get('/registrar', (req,res)=>{
-    Veiculo.findAll().then(function(veiculo){
+    Veiculo.findAll({where: {VEI_Disponibilidade: 1}}).then(function(veiculo){
         Cliente.findAll().then((cliente)=>{
             Venda.findAll().then((venda)=>{
-                res.render('vendas', {veiculo: veiculo, cliente: cliente, venda: venda});
+                res.render('registrarVenda', {veiculo: veiculo, cliente: cliente, venda: venda});
                 console.log(veiculo[0]);
                 console.log(cliente[0]);
                 console.log(venda[0]);
@@ -20,6 +31,297 @@ router.get('/registrar', (req,res)=>{
         
     });
 })
+
+
+
+
+router.get('/vendasRealizadas', (req,res)=>{
+    Venda.findAll().then((vendas)=>{
+        let listaVendas = [];
+        let idVeiculoVendido = [];
+        let idCliente = [];
+        let lista = []
+        let lista2 = []
+
+        for(let i =0; i<vendas.length; i++){
+            idVeiculoVendido.push(vendas[i].VEN_IdVeiculo);
+            idCliente.push(vendas[i].VEN_IdCliente);
+
+            listaVendas.push({
+                'IdVenda': vendas[i].VEN_IdVenda,
+                'IdFuncionario': vendas[i].VEN_IdFunc,
+                'DtdVenda': vendas[i].VEN_DtdVenda,
+                'IdCliente': vendas[i].VEN_IdCliente,
+                'IdVeiculo': vendas[i].VEN_IdVeiculo,
+                'Entrada': vendas[i].VEN_ValorEntrada,
+                'Financiamento': vendas[i].VEN_ValorFinan,
+                'QtdParcela': vendas[i].VEN_QtdParcela,
+                'ValorParcela': vendas[i].VEN_ValorParcela
+            });
+        }
+
+       
+
+        Veiculo.findAll({where:{VEI_IdVeiculo: idVeiculoVendido}}).then((veiculos)=>{
+            for(let i = 0;i<listaVendas.length;i++){
+                for(let j=0;j<veiculos.length;j++){
+                    if(listaVendas[i].IdVeiculo == veiculos[j].VEI_IdVeiculo){
+                        lista.push({
+                            'IdVenda': listaVendas[i].IdVenda,
+                            'IdFuncionario': listaVendas[i].IdFuncionario,
+                            'DtdVenda': listaVendas[i].DtdVenda,
+                            'IdCliente': listaVendas[i].IdCliente,
+                            'IdVeiculo': listaVendas[i].IdVeiculo,
+                            'Entrada': listaVendas[i].Entrada,
+                            'Financiamento': listaVendas[i].Financiamento,
+                            'QtdParcela': listaVendas[i].QtdParcela,
+                            'ValorParcela': listaVendas[i].ValorParcela,
+                            'Marca': veiculos[j].VEI_Marca,
+                            'Modelo': veiculos[j].VEI_Modelo,
+                            'Renavam': veiculos[j].VEI_NumRenavam,
+                            'Placa': veiculos[j].VEI_Placa,
+                            'Cor': veiculos[j].VEI_Cor,
+                            'Ano': veiculos[j].VEI_Ano,
+                            'Situacao': veiculos[j].VEI_Novo,
+                            'Km': veiculos[j].VEI_Km,
+                            'Combustivel': veiculos[j].VEI_Combustivel,
+                            'Valor': veiculos[j].VEI_Valor
+                        });
+                    }
+                }lista
+            }
+            Cliente.findAll({where: {CLI_IdCliente: idCliente}}).then((clientes)=>{
+                for(let i = 0;i<lista.length;i++){
+                    for(let j=0;j<clientes.length;j++){
+                        if(lista[i].IdCliente == clientes[j].CLI_IdCliente){
+                            lista2.push({
+                                'IdVenda': lista[i].IdVenda,
+                                'IdFuncionario': lista[i].IdFuncionario,
+                                'DtdVenda': lista[i].DtdVenda,
+                                'IdCliente': lista[i].IdCliente,
+                                'IdVeiculo': lista[i].IdVeiculo,
+                                'Entrada': lista[i].Entrada,
+                                'Financiamento': lista[i].Financiamento,
+                                'QtdParcela': lista[i].QtdParcela,
+                                'ValorParcela': lista[i].ValorParcela,
+                                'Marca': lista[i].Marca,
+                                'Modelo': lista[i].Modelo,
+                                'Renavam': lista[i].Renavam,
+                                'Placa': lista[i].Placa,
+                                'Cor': lista[i].Cor,
+                                'Ano': lista[i].Ano,
+                                'Situacao': lista[i].Situacao,
+                                'Km': lista[i].Km,
+                                'Combustivel': lista[i].Combustivel,
+                                'Valor': lista[i].Valor,
+                                'NomeCliente': clientes[j].CLI_Nome,
+                                'Cpf': clientes[j].CLI_NumCPF
+                            });
+                        }
+                    }
+                }
+
+                
+                console.log(lista)
+                console.log(lista2)
+                res.render('vendasRealizadas', {vendas: lista2});
+            });
+        });   
+    });
+});
+
+
+
+
+
+
+
+
+
+
+/*
+router.get('/vendasRealizadas', (req,res)=>{
+    Venda.findAll({
+        attributes: ['VEN_IdVenda'],
+        include: [{model: Veiculo, attributes: ['VEI_Modelo'], required: false}],            
+        where:{VEN_IdVenda: 9}
+    }).then((venda)=>{
+        console.log(JSON.stringify(venda, null, 2))
+        res.render('vendasRealizadas',{vendas: venda})
+        let idVeiculoVendido = []
+        for(let i =0; i<vendas.length; i++){
+            idVeiculoVendido.push(vendas[i].VEV_IdVeiculo)
+        }
+        let idCliente = []
+        for(let i =0; i<vendas.length; i++){
+            idVeiculoVendido.push(vendas[i].idCliente)
+        }
+
+        console.log(idVeiculoVendido);
+        Veiculo.findAll({where:{VEI_IdVeiculo: idVeiculoVendido}}).then((veiculos)=>{
+            Cliente.findAll({where: {CLI_IdCliente: idCliente}}).then((clientes)=>{
+                res.render('vendasRealizadas', {vendas: vendas, veiculos: veiculos, clientes: clientes});
+            })
+        })
+    })
+
+})
+
+
+
+
+
+
+
+
+
+router.get('/vendasRealizadas', (req,res)=>{
+    VeiculoVendido.findAll().then((vendas)=>{ 
+
+        let idVeiculoVendido = []
+            for(let i =0; i<vendas.length; i++){
+                idVeiculoVendido.push(vendas[i].VEV_IdVeiculo)
+            }
+            console.log(idVeiculoVendido);
+
+        Veiculo.findAll({where: {VEI_IdVeiculo: idVeiculoVendido}}).then(function(veiculo){
+          //  console.log(veiculo[0]);
+            Cliente.findAll().then((cliente)=>{
+               // console.log(cliente[0]);
+                Venda.findAll().then((venda)=>{
+                   // console.log(venda[0]);
+                    let listaVendas = []
+                    for(let i=0;i<venda.length;i++){
+                        listaVendas.push({
+                            'IdVenda': venda[i].VEN_IdVenda,
+                            'IdFuncionario': venda[i].VEN_IdFunc,
+                            'DtdVenda': venda[i].VEN_DtdVenda,
+                            'IdCliente': venda[i].VEN_IdCliente,
+                            'NomeCliente': cliente[i].CLI_Nome,
+                            'Cpf': cliente[i].CLI_NumCPF,
+                            'IdVeiculo': venda[i].VEN_IdVeiculo,
+                            'Marca': veiculo[i].VEI_Marca,
+                            'Modelo': veiculo[i].VEI_Modelo,
+                            'Renavam': veiculo[i].VEI_NumRenavam,
+                            'Placa': veiculo[i].VEI_Placa,
+                            'Cor': veiculo[i].VEI_Cor,
+                            'Ano': veiculo[i].VEI_Ano,
+                            'Situacao': veiculo[i].VEI_Novo,
+                            'Km': veiculo[i].VEI_Km,
+                            'Combustivel': veiculo[i].VEI_Combustivel,
+                            'Valor': veiculo[i].VEI_Valor,
+                            'Entrada': venda[i].VEN_ValorEntrada,
+                            'Financiamento': venda[i].VEN_ValorFinan,
+                            'QtdParcela': venda[i].VEN_QtdParcela,
+                            'ValorParcela': venda[i].VEN_ValorParcela
+                        })
+                    }
+                    //console.log(listaVendas)
+                    res.render('vendasRealizadas', {vendas: listaVendas});
+                })
+            })
+            
+        });
+    })
+})*/
+
+router.get('/vendaRegistrada/:id', (req,res)=>{
+    let idVenda = req.params.id
+    Venda.findAll({where:{VEN_IdVenda: idVenda}}).then((vendas)=>{
+        let listaVendas = [];
+        let idVeiculoVendido = [];
+        let idCliente = [];
+        let lista = []
+        let lista2 = []
+
+        for(let i =0; i<vendas.length; i++){
+            idVeiculoVendido.push(vendas[i].VEN_IdVeiculo);
+            idCliente.push(vendas[i].VEN_IdCliente);
+
+            listaVendas.push({
+                'IdVenda': vendas[i].VEN_IdVenda,
+                'IdFuncionario': vendas[i].VEN_IdFunc,
+                'DtdVenda': vendas[i].VEN_DtdVenda,
+                'IdCliente': vendas[i].VEN_IdCliente,
+                'IdVeiculo': vendas[i].VEN_IdVeiculo,
+                'Entrada': vendas[i].VEN_ValorEntrada,
+                'Financiamento': vendas[i].VEN_ValorFinan,
+                'QtdParcela': vendas[i].VEN_QtdParcela,
+                'ValorParcela': vendas[i].VEN_ValorParcela
+            });
+        }
+
+       
+
+        Veiculo.findAll({where:{VEI_IdVeiculo: idVeiculoVendido}}).then((veiculos)=>{
+            for(let i = 0;i<listaVendas.length;i++){
+                for(let j=0;j<veiculos.length;j++){
+                    if(listaVendas[i].IdVeiculo == veiculos[j].VEI_IdVeiculo){
+                        lista.push({
+                            'IdVenda': listaVendas[i].IdVenda,
+                            'IdFuncionario': listaVendas[i].IdFuncionario,
+                            'DtdVenda': listaVendas[i].DtdVenda,
+                            'IdCliente': listaVendas[i].IdCliente,
+                            'IdVeiculo': listaVendas[i].IdVeiculo,
+                            'Entrada': listaVendas[i].Entrada,
+                            'Financiamento': listaVendas[i].Financiamento,
+                            'QtdParcela': listaVendas[i].QtdParcela,
+                            'ValorParcela': listaVendas[i].ValorParcela,
+                            'Marca': veiculos[j].VEI_Marca,
+                            'Modelo': veiculos[j].VEI_Modelo,
+                            'Renavam': veiculos[j].VEI_NumRenavam,
+                            'Placa': veiculos[j].VEI_Placa,
+                            'Cor': veiculos[j].VEI_Cor,
+                            'Ano': veiculos[j].VEI_Ano,
+                            'Situacao': veiculos[j].VEI_Novo,
+                            'Km': veiculos[j].VEI_Km,
+                            'Combustivel': veiculos[j].VEI_Combustivel,
+                            'Valor': veiculos[j].VEI_Valor
+                        });
+                    }
+                }lista
+            }
+            Cliente.findAll({where: {CLI_IdCliente: idCliente}}).then((clientes)=>{
+                for(let i = 0;i<lista.length;i++){
+                    for(let j=0;j<clientes.length;j++){
+                        if(lista[i].IdCliente == clientes[j].CLI_IdCliente){
+                            lista2.push({
+                                'IdVenda': lista[i].IdVenda,
+                                'IdFuncionario': lista[i].IdFuncionario,
+                                'DtdVenda': lista[i].DtdVenda,
+                                'IdCliente': lista[i].IdCliente,
+                                'IdVeiculo': lista[i].IdVeiculo,
+                                'Entrada': lista[i].Entrada,
+                                'Financiamento': lista[i].Financiamento,
+                                'QtdParcela': lista[i].QtdParcela,
+                                'ValorParcela': lista[i].ValorParcela,
+                                'Marca': lista[i].Marca,
+                                'Modelo': lista[i].Modelo,
+                                'Renavam': lista[i].Renavam,
+                                'Placa': lista[i].Placa,
+                                'Cor': lista[i].Cor,
+                                'Ano': lista[i].Ano,
+                                'Situacao': lista[i].Situacao,
+                                'Km': lista[i].Km,
+                                'Combustivel': lista[i].Combustivel,
+                                'Valor': lista[i].Valor,
+                                'NomeCliente': clientes[j].CLI_Nome,
+                                'Cpf': clientes[j].CLI_NumCPF
+                            });
+                        }
+                    }
+                }
+
+                
+                console.log(lista)
+                console.log(lista2)
+                res.render('vendaRegistrada', {vendas: lista2});
+            });
+        });   
+    });
+})
+
+
 
 router.post('/addVenda', (req,res)=>{
     //DADOS
@@ -122,7 +424,7 @@ router.post('/addVenda', (req,res)=>{
                 VEV_IdVenda: idVenda
             }).then(()=>{
                 console.log("Venda Cadastrada com Sucesso!")
-                res.redirect('../home')
+                res.redirect('../vendas')
             }).catch((error)=>{
                 res.status(404).send("Deu ruim na tabela VeiculosVendidos: " + error);
             });
